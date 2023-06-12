530fc8bc6e0a200b85d6f8b50ec3e337b7525300
hixvm <hixvmx@gmail.com>
2023-06-13 00:14:34 +0100

    add vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,10 +2,10 @@ import '@/styles/globals.css'
 import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-       <Analytics />
-       <Component {...pageProps} />
-    </>
- );
+    return (
+      <>
+         <Analytics />
+         <Component {...pageProps} />
+      </>
+   );
 }
